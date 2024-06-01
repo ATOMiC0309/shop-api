4462b36740d1d01aa839ed0b4480297f75b77f0f
ATOMiC0309 <jmamajonovm916@gmail.com>
2024-06-01 22:09:54 +0500

    added category, product and review api

---
+++ app/models.py
@@ -0,0 +1,36 @@
+from django.db import models
+
+
+# Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=60)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    published = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField(blank=True, null=True)
+    quantity = models.IntegerField()
+    price = models.FloatField()
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    published = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Review(models.Model):
+    author = models.CharField(max_length=60)
+    comment = models.TextField()
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    created = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.author}: {self.comment[:60]}"
